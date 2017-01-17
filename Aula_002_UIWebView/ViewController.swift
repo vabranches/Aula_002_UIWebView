794e451ffda24f03337c258366a8f9bead583b06
Swift <swift@WozMac8.local>
2017-01-17 20:10:04 -0200

    inicial

--- Aula_002_UIWebView/ViewController.swift
+++ Aula_002_UIWebView/ViewController.swift
@@ -1,24 +1,31 @@
-//
-//  ViewController.swift
-//  Aula_002_UIWebView
-//
-//  Created by Swift on 17/01/17.
-//  Copyright © 2017 Swift. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var minhaWebView: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        /*
+         Para que o App tenha acesso, edite o arquivo:
+         - Info.plist
+         - Clique em + ao lado do Dictionary "Information Property List"
+         - Escolha "App Transport Security Settings"
+         - Ao lado deste novo Dictionary, clique em +
+         - Selecione a propriedade "Allow Arbitrary Loads"
+         - Mude o valor para "YES"
+         */
+        
+        
+        let enderecoWebApp = "http://ios.quaddro.com.br"
+        let URLSite = Foundation.URL(string: enderecoWebApp)
+        let requestURLSite = URLRequest(url: URLSite!)
+        
+        minhaWebView.loadRequest(requestURLSite)
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
